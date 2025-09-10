638e900525096e45a3341df85b831531244fba71
GochiStuff <ncfdmytime@gmail.com>
2025-09-11 01:30:00 +0530

    developer

--- components/header.tsx
+++ components/header.tsx
@@ -30,61 +30,30 @@ export default function Header() {
         </div>
 
         {/* Desktop Nav */}
-{/* <nav className="hidden md:flex items-center gap-2 text-sm md:text-base font-medium text-zinc-700">
-  <span className="opacity-90 animate-spin-slow">⚡</span>
-  <Link
-    href="https://cobbic.com"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="flex items-center gap-2 text-orange-600 hover:text-orange-800 transition-colors duration-200 group"
-  >
-    <span className="font-semibold underline underline-offset-4 decoration-wavy">
-      Join the awesome community of Cobbic
-    </span>
-  </Link>
-</nav> */}
+<nav className="hidden md:flex items-center gap-2 text-sm md:text-base font-medium text-zinc-700">
+  <span className="opacity-90 ">Dev</span>
+ <a
+      href={`https://x.com/GochiStuff`}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-zinc-50 hover:text-orange-400 transition"
+      title="Share on Twitter"
+    >
+      <svg
+        width="20"
+        height="20"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path d="M17.53 3H21l-7.19 8.21L22 21h-6.56l-5.18-6.44L4.47 21H1l7.64-8.73L2 3h6.68l4.74 5.91L17.53 3ZM16.3 19h2.13l-5.82-7.23-1.71 1.98L16.3 19ZM5.09 5l5.38 6.69 1.7-1.97L7.36 5H5.09Z" />
+      </svg>
+    </a>
+</nav>
 
 
         {/* Mobile Sidebar */}
         <div className="md:hidden">
-          <Sheet>
-            <SheetTrigger asChild>
-              <button aria-label="Open Menu">
-                <Menu size={28} className="text-zinc-800" />
-              </button>
-            </SheetTrigger>
-
-            <SheetContent side="right" className="w-64 p-6">
-              <SheetHeader>
-                <div className="flex items-center gap-2 mb-6">
-                  <Image
-                    src="/icons/logo.png"
-                    alt="Logo"
-                    width={32}
-                    height={32}
-                  />
-                  <SheetTitle className="text-lg font-semibold tracking-tight text-zinc-900">
-                    AIR DELIVERY
-                  </SheetTitle>
-                </div>
-              </SheetHeader>
-{/* 
-              <nav className="flex flex-col gap-4 text-zinc-700 text-sm font-medium">
-  <Link
-    href="https://cobbic.com"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="flex items-center gap-2 hover:text-blue-700 transition-colors duration-200 group"
-  >
-    <span className="text-blue-600 font-semibold group-hover:underline underline-offset-4 decoration-wavy">
-      Cobbic.com
-    </span>
-    <span className="opacity-70 group-hover:translate-x-1 transition-transform duration-200">🚀</span>
-  </Link>
-</nav> */}
-
-            </SheetContent>
-          </Sheet>
+         
         </div>
 
         {/* Banner */}
